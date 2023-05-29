daaa61400d107b0dd04f3a1c2bfeb06a0f27880b
gitterence <terencejye@gmail.com>
2023-05-29 00:53:01 -0700

    Updates

---
+++ precache-manifest.babafcee4ba1f0ca8b7a860b9884122e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "154d99e6a81cab7500f8",
+    "url": "/robo-friends/static/css/main.237c1719.chunk.css"
+  },
+  {
+    "revision": "154d99e6a81cab7500f8",
+    "url": "/robo-friends/static/js/main.154d99e6.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/robo-friends/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "3bb670d1ff37e2256680",
+    "url": "/robo-friends/static/css/2.0ce9784e.chunk.css"
+  },
+  {
+    "revision": "3bb670d1ff37e2256680",
+    "url": "/robo-friends/static/js/2.3bb670d1.chunk.js"
+  },
+  {
+    "revision": "4960d31df58e746aa831",
+    "url": "/robo-friends/static/js/runtime~main.4960d31d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9ab3093da5681c2b64f9d30d312ddb75",
+    "url": "/robo-friends/index.html"
+  }
+];
